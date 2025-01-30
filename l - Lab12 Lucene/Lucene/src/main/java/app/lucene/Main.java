88590ccce8058f2b86d8aba99470b94325ffea02
Jakub Ciulęba <116065034+Cioo01@users.noreply.github.com>
2025-01-30 22:15:46 +0100

    nadrobienie zaleglosci

--- l - Lab12 Lucene/Lucene/src/main/java/app/lucene/Main.java
+++ l - Lab12 Lucene/Lucene/src/main/java/app/lucene/Main.java
@@ -1,6 +1,7 @@
 package app.lucene;
 
 import org.apache.lucene.analysis.en.EnglishAnalyzer;
+import org.apache.lucene.analysis.pl.PolishAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -22,31 +23,44 @@ public class Main {
 
     public static void main(String[] args) throws IOException, ParseException {
 //        StandardAnalyzer analyzer = new StandardAnalyzer();
-        EnglishAnalyzer analyzer = new EnglishAnalyzer();
+//        EnglishAnalyzer analyzer = new EnglishAnalyzer();
+        PolishAnalyzer analyzer = new PolishAnalyzer();
         Directory directory = new ByteBuffersDirectory();
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
         IndexWriter w = new IndexWriter(directory, config);
 
 
-        w.addDocument(buildDoc("Lucene in Action", "9781473671911"));
-        w.addDocument(buildDoc("Lucene for Dummies", "9780735219090"));
-        w.addDocument(buildDoc("Managing Gigabytes", "9781982131739"));
-        w.addDocument(buildDoc("The Art of Computer Science",
-                "9781250301695"));
-        w.addDocument(buildDoc("Dummy and yummy title", "9780525656161"));
+//        w.addDocument(buildDoc("Lucene in Action", "9781473671911"));
+//        w.addDocument(buildDoc("Lucene for Dummies", "9780735219090"));
+//        w.addDocument(buildDoc("Managing Gigabytes", "9781982131739"));
+//        w.addDocument(buildDoc("The Art of Computer Science",
+//                "9781250301695"));
+//        w.addDocument(buildDoc("Dummy and yummy title", "9780525656161"));
 
-//        w.addDocument(buildDoc("Lucyna w akcji", "9780062316097"));
-//        w.addDocument(buildDoc("Akcje rosną i spadają", "9780385545955"));
-//        w.addDocument(buildDoc("Bo ponieważ", "9781501168007"));
-//        w.addDocument(buildDoc("Naturalnie urodzeni mordercy",
-//                "9780316485616"));
-//        w.addDocument(buildDoc("Druhna rodzi", "9780593301760"));
-//        w.addDocument(buildDoc("Urodzić się na nowo", "9780679777489"));
-//        w.close();
+        w.addDocument(buildDoc("Lucyna w akcji", "9780062316097"));
+        w.addDocument(buildDoc("Akcje rosną i spadają", "9780385545955"));
+        w.addDocument(buildDoc("Bo ponieważ", "9781501168007"));
+        w.addDocument(buildDoc("Naturalnie urodzeni mordercy",
+                "9780316485616"));
+        w.addDocument(buildDoc("Druhna rodzi", "9780593301760"));
+        w.addDocument(buildDoc("Urodzić się na nowo", "9780679777489"));
+        w.close();
 
 //        String querystr = "*:*";
-        String querystr = "dummy";
+//        String querystr = "dummy";
 //        String querystr = "and";
+//        String querystr = "9780062316097";
+//        String querystr = "urodzić";
+//        String querystr = "rodzić";
+//        String querystr = "ro*";
+//        String querystr = "ponieważ";
+//        String querystr = "Lucyna AND akcja";
+//        String querystr = "akcja NOT Lucyna";
+//        String querystr = "naturalnie morderca ~2";
+//        String querystr = "naturalnie morderca ~1";
+//        String querystr = "naturalnie morderca ~0";
+//        String querystr = "naturalne";
+        String querystr = "naturalne~";
         Query q = new QueryParser("title", analyzer).parse(querystr);
 
         int maxHits = 10;
